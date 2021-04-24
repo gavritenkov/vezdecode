79c9dda1044dd69cbebb0cdf1e08030188251b4b
gavritenkov <83133555+gavritenkov@users.noreply.github.com>
2021-04-24 21:07:59 +0300

    Update receive.py

--- 5task/receive.py
+++ 5task/receive.py
@@ -3,7 +3,6 @@ import base64
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-url = 'https://thingspeak.com/channels/1104680/field/1.csv'
 msg = input("Введите зашифрованное сообщение: ")
 password_provided = input("Введите ключ : ")
 password = password_provided.encode()
